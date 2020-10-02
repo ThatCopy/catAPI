5e5100f98cc3f1234f5dd099678a10c194ec3e0b
ThatCopy <62877244+ThatCopy@users.noreply.github.com>
2020-10-02 19:10:44 +0200

    AAAAAAAA

--- rest/controllers/cat-ctrl.js
+++ rest/controllers/cat-ctrl.js
@@ -2,7 +2,7 @@ const Cat = require('../models/cat-model')
 getCat = async (req, res) => {
     let r = await Cat.countDocuments()
     let random = Math.floor(Math.random() * r)
-    res.send((await Cat.findById(random)).toJSON().toString())
+    res.send(JSON.stringify((await Cat.findById(random)).toJSON()))
 }
 
 
