9fae3b4457cf99cce23ab2cb0496412d7a134fc6
David <62877244+ThatCopy@users.noreply.github.com>
2020-10-22 08:57:33 +0200

    Update index.test.js

--- rest/tests/index.test.js
+++ rest/tests/index.test.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 describe("Test the root path", () => {
   it("It should response the GET method", async () => {
     const res = await request(app)
-      .get("/getId/3")
+      .get("/getId/2")
     await expect(res.text).toBe('{"_id":2,"webpurl":"https://i.thatcopy.pw/cat-webp/mB0JQPR.webp","url":"https://i.thatcopy.pw/cat/mB0JQPR.jpg","x":29.59,"y":31.58,"__v":0}');
   }, 60000);
 
