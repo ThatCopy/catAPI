5765e30e3e48e45e03bd957f3551100f9f0d7b87
David <62877244+ThatCopy@users.noreply.github.com>
2020-10-22 08:42:40 +0200

    Update index.test.js

--- rest/tests/index.test.js
+++ rest/tests/index.test.js
@@ -1,17 +1,3 @@
-// const request = require("supertest");
-// const app = require("../app");
-
-// describe("Test the catId path", () => {
-//   test("It should response the GET method", done => {
-//     request(app)
-//       .get("/getId/2")
-//       .then(response => {
-//         expect(response.text).toBe('{"_id":2,"webpurl":"https://i.thatcopy.pw/cat-webp/mB0JQPR.webp","url":"https://i.thatcopy.pw/cat/mB0JQPR.jpg","x":29.59,"y":31.58,"__v":0}');
-//         done();
-//       });
-//   });
-// });
-
 const request = require("supertest");
 const app = require("../app");
 const mongoose = require("mongoose");
@@ -19,7 +5,7 @@ const mongoose = require("mongoose");
 describe("Test the root path", () => {
   it("It should response the GET method", async () => {
     const res = await request(app)
-      .get("/getId/2")
+      .get("/getId/3")
     await expect(res.text).toBe('{"_id":2,"webpurl":"https://i.thatcopy.pw/cat-webp/mB0JQPR.webp","url":"https://i.thatcopy.pw/cat/mB0JQPR.jpg","x":29.59,"y":31.58,"__v":0}');
   }, 60000);
 
